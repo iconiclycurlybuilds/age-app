2cece864786e4d2fce88956feeffb2ab5e96266c
iconiclycurlybuilds <LilyRose4lia@gmail.com>
2025-04-28 17:56:06 -0700

    Update app.py
    i added  years not money numbers

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.title("How old will I be?")
 
-birth_year = st.number_input("What year were you born?")
+birth_year = st.number_input("What year were you born?", step=1, format="%d")
 
-pick_year = st.number_input("Pick a year!")
+pick_year = st.number_input("Pick a year to know your age!", step=1, format="%d")
 
 if st.button("Tell me my age!"):
     age = pick_year - birth_year
